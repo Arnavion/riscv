39fb24b469b900884bb6b49b3d724c178966c4f1
Arnav Singh <me@arnavion.dev>
2024-08-15 01:59:41 -0700

    Support RV32C.

--- tc/sv/rv_cpu.sv
+++ tc/sv/rv_cpu.sv
@@ -2,7 +2,7 @@ module rv_cpu (
 	input bit clock,
 	input bit reset,
 
-	input bit[31:2] pc,
+	input bit[31:1] pc,
 	input bit[31:0] in,
 	input logic[31:0] ram_load_value,
 
@@ -11,7 +11,7 @@ module rv_cpu (
 	output bit ram_store,
 	output logic[2:0] ram_funct3,
 	output logic[31:0] ram_address,
-	output bit[31:2] pcnext
+	output bit[31:1] pcnext
 );
 	wire[4:0] rd;
 	wire[31:0] rd_store_value;
@@ -26,13 +26,22 @@ module rv_cpu (
 		.rs1_value(rs1_load_value), .rs2_value(rs2_load_value)
 	);
 
+	wire decompressor_sigill;
+	wire decompressor_is_compressed;
+	wire[31:0] decompressor_inst;
+	rv_decompressor decompressor (
+		.in(in),
+		.sigill(decompressor_sigill), .is_compressed(decompressor_is_compressed),
+		.out(decompressor_inst)
+	);
+
 	wire decoder_sigill;
 	wire[4:0] opcode;
 	wire[2:0] funct3;
 	wire[6:0] funct7;
 	wire[31:0] imm;
 	rv_decoder decoder (
-		.in(in),
+		.in(decompressor_inst),
 		.sigill(decoder_sigill),
 		.rd(rd), .rs1(rs1), .rs2(rs2),
 		.opcode(opcode), .funct3(funct3), .funct7(funct7),
@@ -40,21 +49,21 @@ module rv_cpu (
 	);
 
 	wire alu_sigill;
-	wire pcnext_lsb;
 	rv_alu alu (
 		.opcode(opcode), .funct3(funct3), .funct7(funct7),
 		.rs1(rs1_load_value), .rs2(rs2_load_value), .imm(imm),
-		.pc(pc), .pcnext_in(pc + 30'b1),
+		.pc(pc), .pcnext_in(pc + 31'(!decompressor_is_compressed) + 31'b1),
 		.ram_load_value(ram_load_value),
 		.sigill(alu_sigill),
-		.pcnext_out({pcnext, pcnext_lsb}),
+		.pcnext_out(pcnext),
 		.rd(rd_store_value),
 		.ram_load(ram_load), .ram_store(ram_store), .ram_funct3(ram_funct3), .ram_address(ram_address)
 	);
 
-	assign halt = decoder_sigill | alu_sigill | pcnext_lsb;
+	assign halt = decompressor_sigill | decoder_sigill | alu_sigill;
 endmodule
 
 `include "rv_alu.sv"
 `include "rv_decoder.sv"
+`include "rv_decompressor.sv"
 `include "rv_x_regs.sv"
