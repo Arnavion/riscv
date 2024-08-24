c99aa512de3934ccbb6b80dbf351679f30979a17
Arnav Singh <me@arnavion.dev>
2024-08-24 11:01:18 -0700

    Support RV64I and RV64C.

--- tc/sv/rv_decoder.sv
+++ tc/sv/rv_decoder.sv
@@ -35,8 +35,8 @@ module rv_decoder (
 
 		if (~sigill) begin
 			unique casez (in[2+:5])
-				// op
-				5'b01100: begin
+				// op, op-32
+				5'b011?0: begin
 					opcode = in[2+:5];
 					funct3 = in[12+:3];
 					funct7 = in[25+:7];
@@ -51,8 +51,8 @@ module rv_decoder (
 				5'b00000,
 				// misc-mem
 				5'b00011,
-				// op-imm
-				5'b00100,
+				// op-imm, op-imm-32
+				5'b001?0,
 				// jalr
 				5'b11001: begin
 					opcode = in[2+:5];
